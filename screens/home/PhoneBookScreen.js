ead33a481710605a12867b9c37673680d18ecc6f
VoMinhPhuong <vominhphuongk2@gmail.com>
2022-09-13 14:52:48 +0700

    "#10 new phonebook"

--- screens/home/PhoneBookScreen.js
+++ screens/home/PhoneBookScreen.js
@@ -1,18 +1,114 @@
-import { View, Text } from 'react-native';
-import GlobalStyle from '../../styles/GlobalStyle';
-import ButtonPrimary from '../../components/buttons/ButtonPrimary';
+import { View, Text, ScrollView } from "react-native";
+import { StyleSheet } from "react-native";
+import { Avatar } from "react-native-elements";
+import Icon from "react-native-vector-icons/Ionicons";
 
 function PhoneBookScreen({ navigation }) {
-    const onMoveScreen = () => {
-        navigation.navigate('');
-    };
+  const onMoveScreen = () => {
+    navigation.navigate("");
+  };
 
-    return (
-        <View style={GlobalStyle.container}>
-            <Text>This is PhoneBook Screen</Text>
-            <ButtonPrimary title="PhoneBook" onPress={onMoveScreen} />
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.user}>
+          <Avatar
+            rounded
+            size={60}
+            source={require("../../assets/hinh-anh-conan.jpg")}
+          ></Avatar>
+          <View style={styles.infoUser}>
+            <Text style={styles.nameUser}>Minh Phuong</Text>
+            <Text style={styles.phoneUser}>+123456</Text>
+          </View>
+          <View style={styles.call}>
+            <Icon style={{marginRight:20}} name="call-outline" color="#000" size={20} />
+            <Icon name="videocam-outline" color="#000" size={20} />
+          </View>
         </View>
-    );
+      </ScrollView>
+      <ScrollView>
+        <View>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+            <Text>A</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    backgroundColor:"#fff",
+    marginTop: 70,
+  },
+  user: {
+    marginTop: 20,
+    alignItems: "center",
+    flexDirection: "row",
+    marginLeft: 20,
+  },
+  infoUser: {
+    width: "50%",
+    margin: 5,
+    marginLeft: 30,
+  },
+  nameUser: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  phoneUser: {
+    paddingTop: 5,
+  },
+  call:{
+    flexDirection:"row",
+  }
+});
 export default PhoneBookScreen;
