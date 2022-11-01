40de9120acd569af9d6081b3eb1a89a9122c3c23
iamtun <letuan19431791.iuh@gmail.com>
2022-11-01 18:53:33 +0700

    #104 - handle sync phone book

--- screens/home/phonebook/SyncPhoneBook.js
+++ screens/home/phonebook/SyncPhoneBook.js
@@ -4,99 +4,109 @@ import { useSelector } from 'react-redux';
 import { userInfoSelector, userListSelector } from '../../../redux/selector';
 import Header from '../../../components/Header';
 import { useState } from 'react';
-import { Alert, FlatList, PermissionsAndroid, Platform } from 'react-native';
+import { Alert, FlatList, Platform } from 'react-native';
 import SearchItem from '../../../components/SearchBar/SearchItem';
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 function SyncPhoneBook({ navigation }) {
     const users = useSelector(userListSelector);
     const me = useSelector(userInfoSelector);
     const [friends, setFriends] = useState([]);
 
-    //get my number phone
-    useEffect(() => {
-        const getPhoneNumbersByDevice = async () => {
-            const phones = [];
-            if (Platform.OS === 'ios') {
-                const { status } = await Contacts.requestPermissionsAsync();
-                if (status === 'granted') {
-                    const { data } = await Contacts.getContactsAsync({
-                        fields: [Contacts.Fields.PhoneNumbers],
-                    });
+    const getPhoneNumbers = async () => {
+        const phones = [];
+        //request permission
+        const { status } = await Contacts.requestPermissionsAsync();
 
-                    if (data.length > 0) {
-                        data.forEach((d) =>
-                            d.phoneNumbers != undefined ? phones.push(d.phoneNumbers[0].digits) : null,
-                        );
-                    }
+        //check permission
+        if (status === 'granted') {
+            //get data
+            const { data } = await Contacts.getContactsAsync({
+                fields: [Contacts.Fields.PhoneNumbers],
+            });
 
-                    return phones;
+            //check data to platform
+            if (data.length > 0) {
+                if (Platform.OS === 'ios') {
+                    data.forEach((d) => (d.phoneNumbers != undefined ? phones.push(d.phoneNumbers[0].digits) : null));
+                } else if (Platform.OS === 'android') {
+                    data.forEach((d) => (d.phoneNumbers != undefined ? phones.push(d.phoneNumbers[0].number) : null));
                 }
             }
+        } else {
+            console.warn('no permission!');
+        }
 
-            if (Platform.OS === 'android') {
-                const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
-                    title: 'Yêu cầu cấp quyền',
-                    message: 'Vui lòng cho phép để truy cập danh bạ của bạn!',
-                    buttonNeutral: 'Ask Me Later',
-                    buttonNegative: 'Cancel',
-                    buttonPositive: 'OK',
-                });
-                console.log(granted);
+        return phones;
+    };
 
-                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                    const { data } = await Contacts.getContactsAsync({
-                        fields: [Contacts.Fields.PhoneNumbers],
-                    });
+    const getPhoneNumbersByDevice = async () => {
+        const statusPermission = await Contacts.getPermissionsAsync();
+        if (statusPermission.status === Contacts.PermissionStatus.DENIED) {
+            const phones = await getPhoneNumbers();
+            return phones;
+        } else {
+            const phones = await getPhoneNumbers();
+            return phones;
+        }
+    };
 
-                    if (data.length > 0) {
-                        data.forEach((d) =>
-                            d.phoneNumbers != undefined ? phones.push(d.phoneNumbers[0].number) : null,
-                        );
-                    }
-
-                    return phones;
-                } else {
-                    console.log('Contacts permission denied');
-                }
-            }
-        };
-
-        const syncPhoneBook = async () => {
-            try {
-                const phones = await getPhoneNumbersByDevice();
+    const syncPhoneBook = async () => {
+        try {
+            const phones = await getPhoneNumbersByDevice();
+            if (phones?.length > 0) {
                 const _friends = users.filter(
                     (user) => phones.includes(user.phoneNumber) && !me.friends.includes(user._id),
                 );
-                console.log(_friends);
                 setFriends(_friends);
-            } catch (error) {
-                Alert.alert('Lỗi đồng bộ', error.message);
+            } else {
+                setFriends(null);
             }
-        };
+        } catch (error) {
+            Alert.alert('Lỗi đồng bộ', error.message);
+        }
+    };
+
+    useEffect(() => {
         syncPhoneBook();
     }, []);
 
     return (
         <>
             <Header />
+            <Text style={styles.title}>Đồng bộ danh bạ</Text>
             <View>
-                <FlatList
-                    data={friends}
-                    renderItem={({ item }) => (
-                        <SearchItem
-                            id={item._id}
-                            name={item.fullName}
-                            phonNumber={item.phoneNumber}
-                            image={item.avatarLink}
-                            isFriend={item.isFriend}
-                            navigation={navigation}
-                        />
-                    )}
-                />
+                {friends?.length > 0 ? (
+                    <FlatList
+                        data={friends}
+                        renderItem={({ item }) => (
+                            <SearchItem
+                                id={item._id}
+                                name={item.fullName}
+                                phonNumber={item.phoneNumber}
+                                image={item.avatarLink}
+                                isFriend={item.isFriend}
+                                navigation={navigation}
+                            />
+                        )}
+                    />
+                ) : (
+                    <Text>Không có bạn bè nào trong danh bạ!</Text>
+                )}
             </View>
         </>
     );
 }
 
+const styles = StyleSheet.create({
+    title: {
+        padding: 16,
+        textAlign: 'center',
+        fontSize: 18,
+        color: "#333",
+        fontWeight: 'bold',
+        borderBottomColor: "#ccc",
+        borderBottomWidth: 1,
+    },
+});
 export default SyncPhoneBook;
